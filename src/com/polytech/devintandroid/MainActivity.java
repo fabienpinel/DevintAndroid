ea870f583acc7b4f5bf42fe7fe4a777aea9e37c6
fabien <pinel.fabien@gmail.com>
2014-03-05 12:13:12 +0100

    modification de quelques couleurs.

--- src/com/polytech/devintandroid/MainActivity.java
+++ src/com/polytech/devintandroid/MainActivity.java
@@ -52,7 +52,9 @@ public class MainActivity extends Activity {
 		
 
 	}
-
+	/**
+	 * Pour l'instant ce menu ne sert pas.
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
