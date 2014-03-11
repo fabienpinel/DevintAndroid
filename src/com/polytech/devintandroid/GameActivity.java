8f1b1a9cb8e1458594abaa63cb19b2958896540c
fabien <pinel.fabien@gmail.com>
2014-03-11 23:44:41 +0100

    avancement capteur de mouvement

--- src/com/polytech/devintandroid/GameActivity.java
+++ src/com/polytech/devintandroid/GameActivity.java
@@ -2,22 +2,97 @@ package com.polytech.devintandroid;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.content.Intent;
-import android.os.Build;
+import android.util.Log;
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class GameActivity extends Activity {
-
+public class GameActivity extends Activity implements SensorEventListener{
+	private SensorManager	sensorManager;
+	private Sensor			accelerometer;
+	//private Intent			intent;
+	private TextView		view_x, view_y, view_z;
+	private float	x, y, z;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
+		gameActivityInit();
+		sensorManager.registerListener(this, accelerometer,
+				SensorManager.SENSOR_DELAY_UI);
+		
 		setContentView(R.layout.activity_game);
-		Intent intent = getIntent();
+		
+		
+		view_x = (TextView) findViewById(R.id.textpos_x);
+		view_y = (TextView) findViewById(R.id.textpos_y);
+		view_z = (TextView) findViewById(R.id.textpos_z);
+		
+		
+		
+		//intent = getIntent();
+	}
+	
+	public void gameActivityInit() {
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+	@Override
+	protected void onPause() {
+		// unregister the sensor (désenregistrer le capteur)
+		sensorManager.unregisterListener((SensorEventListener) this, accelerometer);
+		super.onPause();
 	}
 
+	@Override
+	protected void onResume() {
+		sensorManager.registerListener(this, accelerometer,
+				SensorManager.SENSOR_DELAY_UI);
+		super.onResume();
+	}
+	
 
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setZ(float z) {
+		this.z = z;
+	}
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			this.x = event.values[0];
+			this.y = event.values[1];
+			this.z = event.values[2];
+		}
+		Log.d("Log.INFO", "Sensor's values ("+x+","+y+","+z+")");
+		view_x.setText(""+this.getX());
+		view_y.setText(""+this.getY());
+		view_z.setText(""+this.getZ());
+	}
 
 }
--- src/com/polytech/devintandroid/MainActivity.java
+++ src/com/polytech/devintandroid/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -13,45 +12,51 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
 
 		/*
-		 * Ajout du listener sur le bouton start pour charger l'activité StartActivity
+		 * Ajout du listener sur le bouton start pour charger l'activité
+		 * StartActivity
 		 */
 		Button startButton = (Button) findViewById(R.id.startButton);
 		startButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent gameView = new Intent(MainActivity.this, GameActivity.class);
+				Intent gameView = new Intent(MainActivity.this,
+						GameActivity.class);
 				startActivity(gameView);
 			}
 		});
 		/*
-		 * Ajout du listener sur le bouton Options pour charger l'activité OptionsActivity
+		 * Ajout du listener sur le bouton Options pour charger l'activité
+		 * OptionsActivity
 		 */
 		Button optionsButton = (Button) findViewById(R.id.optionsButton);
 		optionsButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent optionsView = new Intent(MainActivity.this, OptionsActivity.class);
+				Intent optionsView = new Intent(MainActivity.this,
+						OptionsActivity.class);
 				startActivity(optionsView);
 			}
 		});
 		/*
-		 * Ajout du listener sur le bouton Help pour charger l'activité HelpActivity
+		 * Ajout du listener sur le bouton Help pour charger l'activité
+		 * HelpActivity
 		 */
 		Button helpButton = (Button) findViewById(R.id.helpButton);
 		helpButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent helpView = new Intent(MainActivity.this, HelpActivity.class);
+				Intent helpView = new Intent(MainActivity.this,
+						HelpActivity.class);
 				startActivity(helpView);
 			}
 		});
-		
-		
 
 	}
+
 	/**
 	 * Pour l'instant ce menu ne sert pas.
 	 */
