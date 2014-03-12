c415f0f429f32d47a11e574d08f1170340355714
fabien <pinel.fabien@gmail.com>
2014-03-12 09:52:45 +0100

    Affichage capteur accelerometre

--- src/com/polytech/devintandroid/GameActivity.java
+++ src/com/polytech/devintandroid/GameActivity.java
@@ -3,48 +3,52 @@ package com.polytech.devintandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
-public class GameActivity extends Activity implements SensorEventListener{
+public class GameActivity extends Activity implements SensorEventListener {
 	private SensorManager	sensorManager;
 	private Sensor			accelerometer;
-	//private Intent			intent;
+	// private Intent intent;
 	private TextView		view_x, view_y, view_z;
-	private float	x, y, z;
-	
+	private float			x, y, z;
+	private Display			display;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		
+
 		gameActivityInit();
 		sensorManager.registerListener(this, accelerometer,
 				SensorManager.SENSOR_DELAY_UI);
-		
+
 		setContentView(R.layout.activity_game);
-		
-		
+		display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+
 		view_x = (TextView) findViewById(R.id.textpos_x);
 		view_y = (TextView) findViewById(R.id.textpos_y);
 		view_z = (TextView) findViewById(R.id.textpos_z);
-		
-		
-		
-		//intent = getIntent();
+
+		// intent = getIntent();
 	}
-	
+
 	public void gameActivityInit() {
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    }
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		accelerometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	}
+
 	@Override
 	protected void onPause() {
 		// unregister the sensor (désenregistrer le capteur)
-		sensorManager.unregisterListener((SensorEventListener) this, accelerometer);
+		sensorManager.unregisterListener((SensorEventListener) this,
+				accelerometer);
 		super.onPause();
 	}
 
@@ -54,7 +58,6 @@ public class GameActivity extends Activity implements SensorEventListener{
 				SensorManager.SENSOR_DELAY_UI);
 		super.onResume();
 	}
-	
 
 	public float getX() {
 		return x;
@@ -79,20 +82,41 @@ public class GameActivity extends Activity implements SensorEventListener{
 	public void setZ(float z) {
 		this.z = z;
 	}
+
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
 
 	}
+
 	public void onSensorChanged(SensorEvent event) {
 		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			this.x = event.values[0];
-			this.y = event.values[1];
+			// En fonction de l'orientation de l'appareil, on corrige les
+			// valeurs x et y du capteur
+			switch (display.getRotation()) {
+			case Surface.ROTATION_0:
+				this.x = event.values[0];
+				this.y = event.values[1];
+				break;
+			case Surface.ROTATION_90:
+				this.x = -event.values[1];
+				this.y = event.values[0];
+				break;
+			case Surface.ROTATION_180:
+				this.x = -event.values[0];
+				this.y = -event.values[1];
+				break;
+			case Surface.ROTATION_270:
+				this.x = event.values[1];
+				this.y = -event.values[0];
+				break;
+			}
+			// la valeur de z
 			this.z = event.values[2];
 		}
-		Log.d("Log.INFO", "Sensor's values ("+x+","+y+","+z+")");
-		view_x.setText(""+this.getX());
-		view_y.setText(""+this.getY());
-		view_z.setText(""+this.getZ());
+		Log.d("Log.INFO", "Sensor's values (" + x + "," + y + "," + z + ")");
+		view_x.setText("x: " + this.getX());
+		view_y.setText("y: " + this.getY());
+		view_z.setText("z: " + this.getZ());
 	}
 
 }
