a7830aebf0420d4ad75240a4fd1a625fb0d25b74
fabien <pinel.fabien@gmail.com>
2014-03-12 13:31:15 +0100

    drawing not working

---
+++ src/com/polytech/devintandroid/Draw.java
@@ -0,0 +1,91 @@
+package com.polytech.devintandroid;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView.Renderer;
+
+public class Draw implements Renderer {
+	
+		
+		public static int loadShader(int type, String shaderCode){
+
+		    // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+		    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+		    int shader = GLES20.glCreateShader(type);
+
+		    // add the source code to the shader and compile it
+		    GLES20.glShaderSource(shader, shaderCode);
+		    GLES20.glCompileShader(shader);
+
+		    return shader;
+		}
+	
+	public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+		gl10.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+
+		gl10.glClearColor(0.0f, 0.0f, 0.0f, 1);
+
+		// Enable Flat Shading.
+
+		gl10.glShadeModel(GL10.GL_FLAT);
+
+		// we don't need to worry about depth testing!
+
+		gl10.glDisable(GL10.GL_DEPTH_TEST);
+
+		// Set OpenGL to optimise for 2D Textures
+
+		gl10.glEnable(GL10.GL_TEXTURE_2D);
+
+		// Disable 3D specific features.
+
+		// but you'll have to figure that out for yourself.
+
+		gl10.glDisable(GL10.GL_DITHER);
+
+		gl10.glDisable(GL10.GL_LIGHTING);
+
+		gl10.glTexEnvx(GL10.GL_TEXTURE_ENV, GL10.GL_TEXTURE_ENV_MODE,
+				GL10.GL_MODULATE); // Initial clear of the screen.
+
+		gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		// initialize a triangle
+	    Triangle triangle = new Triangle();
+
+	}
+
+	public void onDrawFrame(GL10 gl10) {
+
+		// just clear the screen and depth buffer.
+
+		gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+	}
+
+	public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+		gl10.glViewport(0, 0, i, i1);
+
+		/*
+		 * 
+		 * Set our projection matrix. This doesn't have to be done each time we
+		 * 
+		 * draw, but usually a new projection needs to be set when the viewport
+		 * 
+		 * is resized.
+		 */
+
+		float ratio = (float) i / i1;
+
+		gl10.glMatrixMode(GL10.GL_PROJECTION);
+
+		gl10.glLoadIdentity();
+
+		gl10.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+
+	}
+	
+}
--- src/com/polytech/devintandroid/GameActivity.java
+++ src/com/polytech/devintandroid/GameActivity.java
@@ -1,5 +1,11 @@
 package com.polytech.devintandroid;
 
+import java.util.Random;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.EGLConfig;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -7,12 +13,14 @@ import android.view.Display;
 import android.view.Surface;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
-public class GameActivity extends Activity implements SensorEventListener {
+public class GameActivity extends Activity  {
 	private SensorManager	sensorManager;
 	private Sensor			accelerometer;
 	// private Intent intent;
@@ -25,11 +33,15 @@ public class GameActivity extends Activity implements SensorEventListener {
 		super.onCreate(savedInstanceState);
 
 		gameActivityInit();
-		sensorManager.registerListener(this, accelerometer,
-				SensorManager.SENSOR_DELAY_UI);
+		/*sensorManager.registerListener(this, accelerometer,
+				SensorManager.SENSOR_DELAY_UI);*/
+		GLSurfaceView view = new GLSurfaceView(this);
 
-		setContentView(R.layout.activity_game);
-		display = ((WindowManager) getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+		view.setRenderer(new Draw());
+		setContentView(view);
+		// setContentView(R.layout.activity_game);
+		display = ((WindowManager) getSystemService(WINDOW_SERVICE))
+				.getDefaultDisplay();
 
 		view_x = (TextView) findViewById(R.id.textpos_x);
 		view_y = (TextView) findViewById(R.id.textpos_y);
@@ -39,23 +51,21 @@ public class GameActivity extends Activity implements SensorEventListener {
 	}
 
 	public void gameActivityInit() {
-		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		/*sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		accelerometer = sensorManager
-				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);*/
 	}
 
 	@Override
 	protected void onPause() {
 		// unregister the sensor (désenregistrer le capteur)
-		sensorManager.unregisterListener((SensorEventListener) this,
-				accelerometer);
+		//sensorManager.unregisterListener((SensorEventListener) this,accelerometer);
 		super.onPause();
 	}
 
 	@Override
 	protected void onResume() {
-		sensorManager.registerListener(this, accelerometer,
-				SensorManager.SENSOR_DELAY_UI);
+		//sensorManager.registerListener(this, accelerometer,SensorManager.SENSOR_DELAY_UI);
 		super.onResume();
 	}
 
@@ -88,35 +98,97 @@ public class GameActivity extends Activity implements SensorEventListener {
 
 	}
 
-	public void onSensorChanged(SensorEvent event) {
-		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			// En fonction de l'orientation de l'appareil, on corrige les
-			// valeurs x et y du capteur
-			switch (display.getRotation()) {
-			case Surface.ROTATION_0:
-				this.x = event.values[0];
-				this.y = event.values[1];
-				break;
-			case Surface.ROTATION_90:
-				this.x = -event.values[1];
-				this.y = event.values[0];
-				break;
-			case Surface.ROTATION_180:
-				this.x = -event.values[0];
-				this.y = -event.values[1];
-				break;
-			case Surface.ROTATION_270:
-				this.x = event.values[1];
-				this.y = -event.values[0];
-				break;
+	/*public void onSensorChanged(SensorEvent event) {
+		if (event != null && display != null) {
+			if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+				// En fonction de l'orientation de l'appareil, on corrige les
+				// valeurs x et y du capteur
+				switch (display.getRotation()) {
+				case Surface.ROTATION_0:
+					this.x = event.values[0];
+					this.y = event.values[1];
+					break;
+				case Surface.ROTATION_90:
+					this.x = -event.values[1];
+					this.y = event.values[0];
+					break;
+				case Surface.ROTATION_180:
+					this.x = -event.values[0];
+					this.y = -event.values[1];
+					break;
+				case Surface.ROTATION_270:
+					this.x = event.values[1];
+					this.y = -event.values[0];
+					break;
+				}
+				// la valeur de z
+				this.z = event.values[2];
+				view_x.setText("x: " + this.getX());
+				view_y.setText("y: " + this.getY());
+				view_z.setText("z: " + this.getZ());
 			}
-			// la valeur de z
-			this.z = event.values[2];
 		}
-		Log.d("Log.INFO", "Sensor's values (" + x + "," + y + "," + z + ")");
-		view_x.setText("x: " + this.getX());
-		view_y.setText("y: " + this.getY());
-		view_z.setText("z: " + this.getZ());
+		// Log.d("Log.INFO", "Sensor's values (" + x + "," + y + "," + z + ")");
+
+	}*/
+
+	public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+		gl10.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST); 
+		gl10.glClearColor(0.0f, 0.0f, 0.0f, 1);
+		// Enable Flat Shading.
+
+		gl10.glShadeModel(GL10.GL_FLAT);
+
+		// we don't need to worry about depth testing!
+
+		gl10.glDisable(GL10.GL_DEPTH_TEST);
+
+		// Set OpenGL to optimise for 2D Textures
+
+		gl10.glEnable(GL10.GL_TEXTURE_2D);
+
+		// Disable 3D specific features.
+
+		// but you'll have to figure that out for yourself.
+
+		gl10.glDisable(GL10.GL_DITHER);
+
+		gl10.glDisable(GL10.GL_LIGHTING);
+
+		gl10.glTexEnvx(GL10.GL_TEXTURE_ENV, GL10.GL_TEXTURE_ENV_MODE,
+				GL10.GL_MODULATE); // Initial clear of the screen.
+
+		gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
 	}
 
+	public void onDrawFrame(GL10 gl10) {
+
+		// just clear the screen and depth buffer.
+
+		gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+	}
+
+	public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+		gl10.glViewport(0, 0, i, i1);
+
+		/*
+		 * Set our projection matrix. This doesn't have to be done each time we
+		 * 
+		 * draw, but usually a new projection needs to be set when the viewport
+		 * 
+		 * is resized.
+		 */
+		float ratio = (float) i / i1;
+
+		gl10.glMatrixMode(GL10.GL_PROJECTION);
+
+		gl10.glLoadIdentity();
+
+		gl10.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+
+	}
 }
