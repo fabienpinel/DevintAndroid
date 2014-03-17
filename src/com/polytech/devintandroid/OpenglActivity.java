d0bc851c05575764a5c3fd4e81612c54043e6c19
fabien <pinel.fabien@gmail.com>
2014-03-17 18:16:08 +0100

    ontouchevent

--- src/com/polytech/devintandroid/OpenglActivity.java
+++ src/com/polytech/devintandroid/OpenglActivity.java
@@ -6,39 +6,47 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.support.v4.app.NavUtils;
 
 public class OpenglActivity extends Activity {
-	private GLSurfaceView mGLSurfaceView;
+	private GLSurfaceView	mGLSurfaceView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_opengl);
+		// setContentView(R.layout.activity_opengl);
 		mGLSurfaceView = new GLSurfaceView(this);
-		 
-	    // Check if the system supports OpenGL ES 2.0.
-	    final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-	    final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
-	    final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
-	 
-	    if (supportsEs2)
-	    {
-	        // Request an OpenGL ES 2.0 compatible context.
-	        mGLSurfaceView.setEGLContextClientVersion(2);
-	 
-	        // Set the renderer to our demo renderer, defined below.
-	        mGLSurfaceView.setRenderer(new MyRenderer(this));
-	    }
-	    else
-	    {
-	        // This is where you could create an OpenGL ES 1.x compatible
-	        // renderer if you wanted to support both ES 1 and ES 2.
-	        return;
-	    }
-	 
-	    setContentView(mGLSurfaceView);
+
+		// Check if the system supports OpenGL ES 2.0.
+		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		final ConfigurationInfo configurationInfo = activityManager
+				.getDeviceConfigurationInfo();
+		final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+		if (supportsEs2) {
+			// Request an OpenGL ES 2.0 compatible context.
+			mGLSurfaceView.setEGLContextClientVersion(2);
+
+			// Set the renderer to our demo renderer, defined below.
+			mGLSurfaceView.setRenderer(new MyRenderer(this));
+		} else {
+			// This is where you could create an OpenGL ES 1.x compatible
+			// renderer if you wanted to support both ES 1 and ES 2.
+			return;
+		}
+
+		setContentView(mGLSurfaceView);
 	}
 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.d("RunGameActivity", "OnTouchEvent");
+		synchronized (this) {
+		}
+		return true;
+	}
 }
