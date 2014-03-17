325570ead39e19313665a852dd7f30f0cfd41fda
fabien <pinel.fabien@gmail.com>
2014-03-17 18:09:21 +0100

    don't know ??

--- src/com/polytech/devintandroid/MyRenderer.java
+++ src/com/polytech/devintandroid/MyRenderer.java
@@ -15,6 +15,27 @@ import android.opengl.Matrix;
 import android.os.SystemClock;
 
 public class MyRenderer implements GLSurfaceView.Renderer {
+	 // Program variables
+    public static int sp_SolidColor;
+ 
+    /* SHADER Solid
+     *
+     * This shader is for rendering a colored primitive.
+     *
+     */
+    public static final String vs_SolidColor =
+        "uniform    mat4        uMVPMatrix;" +
+        "attribute  vec4        vPosition;" +
+        "void main() {" +
+        "  gl_Position = uMVPMatrix * vPosition;" +
+        "}";
+ 
+    public static final String fs_SolidColor =
+        "precision mediump float;" +
+        "void main() {" +
+        "  gl_FragColor = vec4(0.5,0,0,1);" +
+        "}";
+    
     // Our matrices
     private final float[] mtrxProjection = new float[16];
     private final float[] mtrxView = new float[16];
@@ -183,26 +204,7 @@ public class MyRenderer implements GLSurfaceView.Renderer {
  
     }
 
-    // Program variables
-    public static int sp_SolidColor;
- 
-    /* SHADER Solid
-     *
-     * This shader is for rendering a colored primitive.
-     *
-     */
-    public static final String vs_SolidColor =
-        "uniform    mat4        uMVPMatrix;" +
-        "attribute  vec4        vPosition;" +
-        "void main() {" +
-        "  gl_Position = uMVPMatrix * vPosition;" +
-        "}";
- 
-    public static final String fs_SolidColor =
-        "precision mediump float;" +
-        "void main() {" +
-        "  gl_FragColor = vec4(0.5,0,0,1);" +
-        "}";
+   
  
     public static int loadShader(int type, String shaderCode){
  
